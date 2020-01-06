fc19ccb9f02c50232cb4c6d058e3feb9fc460203
mariam <mariammenaem10@gmail.com>
2020-01-06 23:56:10 +0200

    add login contrloler methods

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ Route::delete('/posts/{post}','PostController@destroy')->name('posts.destroy');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
